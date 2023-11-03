b11443f05d2abd80f90f4f48c091b48353afc64f
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-11-03 22:35:11 +0900

    !(^^)!

--- js/fadein.js
+++ js/fadein.js
@@ -1,3 +1,4 @@
+// Scrollイベントの度にスクロール位置を判定してクラスを付けたり外したりしたい
 $(window).scroll(function () {
 	fadeAnime();
 });
@@ -8,24 +9,26 @@ $(window).on("load", function () {
 
 function fadeAnime() {
 	$(".fadeInUp").each(function () {
-		//fadeUpTriggerというクラス名が
-		var elemPos = $(this).offset().top - 50; //要素より、50px上の
+		//$(this)はセレクターで指定したクラス(この場合fadeUpクラス)
+		var elemPos = $(this).offset().top - 50; //セレクターのクラスの50px手前の高さ
 		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
+		var windowHeight = $(window).height(); // 画面分の高さ
+
+		// 指定した要素よりも現在のスクロール位置が高ければクラス付加、低ければクラス削除
 		if (scroll >= elemPos - windowHeight) {
-			$(this).addClass("animate__fadeInUp animate__animated"); // 画面内に入ったらfadeUpというクラス名を追記
+			$(this).addClass("animate__fadeInUp animate__animated");
 		} else {
-			$(this).removeClass("animate__fadeInUp animate__animated"); // 画面外に出たらfadeUpというクラス名を外す
+			$(this).removeClass("animate__fadeInUp animate__animated");
 		}
 	});
-	$(".headShake").each(function () {
+	$(".swing").each(function () {
 		var elemPos = $(this).offset().top - 50;
 		var scroll = $(window).scrollTop();
 		var windowHeight = $(window).height();
 		if (scroll >= elemPos - windowHeight) {
-			$(this).addClass("animate__animated animate__headShake");
+			$(this).addClass("animate__animated animate__swing");
 		} else {
-			$(this).removeClass("animate__animated animate__headShake");
+			$(this).removeClass("animate__animated animate__swing");
 		}
 	});
 	$(".tada").each(function () {
@@ -36,6 +39,46 @@ function fadeAnime() {
 			$(this).addClass("animate__animated animate__tada");
 		} else {
 			$(this).removeClass("animate__animated animate__tada");
+		}
+	});
+	$(".rubberBand").each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("animate__animated animate__rubberBand");
+		} else {
+			$(this).removeClass("animate__animated animate__rubberBand");
+		}
+	});
+	$(".fadeIn").each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("animate__animated animate__fadeIn");
+		} else {
+			$(this).removeClass("animate__animated animate__fadeIn ");
+		}
+	});
+	$(".fadeInRight").each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("animate__animated animate__fadeInRight");
+		} else {
+			$(this).removeClass("animate__animated animate__fadeInRight");
+		}
+	});
+	$(".fadeInLeft").each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("animate__animated animate__fadeInLeft");
+		} else {
+			$(this).removeClass("animate__animated animate__fadeInLeft");
 		}
 	});
 }
